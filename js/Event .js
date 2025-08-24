c030ab8d6b05aad1dd482762d1dbba0100f9d900
Mahidi-Hasan-Supon <mahidihasansupon@gmail.com>
2025-08-24 12:39:36 +0600

    added

--- js/Event .js
+++ js/Event .js
@@ -1,1 +1,9 @@
 console.log('hello world')
+
+    function makeYellow(){
+            document.body.style.background = 'yellow'
+        }
+
+    function makeRed(){
+            document.body.style.background='red'
+        }
